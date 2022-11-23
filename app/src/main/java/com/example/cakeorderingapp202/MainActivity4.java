674d64615beef8769c1d67ee5891a86677a7ac68
Bhuvaneshvar <bhuvaneshvar8@gmail.com>
2022-11-23 21:58:45 +0530

    details screen data

--- app/src/main/java/com/example/cakeorderingapp202/MainActivity4.java
+++ app/src/main/java/com/example/cakeorderingapp202/MainActivity4.java
@@ -4,23 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
     TextView invoice;
+    TextView contactDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-        invoice = findViewById(R.id.textView2);
+        invoice = findViewById(R.id.tv_package);
+        contactDetails = findViewById(R.id.tv_contact_details);
+        findViewById(R.id.btn_home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity4.this, MainActivity2.class));
+                finishAffinity();
+            }
+        });
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         String phone_number = intent.getStringExtra("phone");
         String address = intent.getStringExtra("address");
         String result1 = intent.getStringExtra("result");
         String date = intent.getStringExtra("date");
-        invoice.setText(new StringBuilder().append("Customer Name : ").append(name).append("\n").append("Phone Number : ").append(phone_number).append("\n").append("Address : ").append(address).append("\n").append(result1).append("\n").append("Delivery Date : ").append(date).append("\n").toString());
-
+        contactDetails.setText(getString(R.string.contact_details, name, phone_number, address, date));
+        invoice.setText(getString(R.string.delivery_content, result1));
     }
 }
